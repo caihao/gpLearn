1035f581116c35801c9b13e11acbe146e6a926dc
chengaoyan <aoyancheng@163.com>
2023-07-17 10:30:40 +0800

    stable-1.0.1

--- main.py
+++
@@ -1,7 +0,0 @@
-from bin.automation import Au
-
-au=Au([100,150,200],[],100,100,[4],limit_min_pix_number=True,ignore_head_number=0,batch_size=1,use_data_type="nolimit_3",centering=True,use_weight=True,train_type="position",need_data_info=True)
-au.load_model("PointNet_test.pt","PointNet",True)
-au.train_step([1,1],[6e-6,2e-6])
-au.test()
-au.finish()
